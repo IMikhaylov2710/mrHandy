0cb203e4cfca6203e616acae68ad07ea12a9c098
IMikhaylov2710 <mikhailovivan27@gmail.com>
2024-05-16 15:14:22 +0300

    OOP update

--- README.md
+++ README.md
@@ -6,11 +6,6 @@ Installation:
 2) cd into folder
 3) run pip install requirements.txt
 
-Optionally you can make a macOS application by:
-
-1) cd into folder and run python setup.py py2app, which would make a standalone app
-2) then go to dist and click into application, instruction is at [Official py2app documentations](https://py2app.readthedocs.io/en/latest/)
-
 Running script:
 
 1) run python face.py
@@ -19,4 +14,6 @@ Running script:
 By default this script recogizes two hands
 
 Whats new?
-Added py2app to requirements.txt, therefor no need for additional installations
+1) changed script to match OOP style
+2) updated gitignore to standard
+3) removed py2app
--- face.py
+++ face.py
@@ -7,58 +7,9 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
+from helpers.helperFunctions import draw_landmarks_on_image, get_annotation_from
 
 vid = cv2.VideoCapture(0) 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-model_path = os.path.join(dir_path, 'hand_landmarker.task')
-
-MARGIN = 10 
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54)
-
-def draw_landmarks_on_image(rgb_image, detection_result):
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
-    annotated_image = np.copy(rgb_image)
-    for idx in range(len(hand_landmarks_list)):
-        hand_landmarks = hand_landmarks_list[idx]
-        handedness = handedness_list[idx]
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        hand_landmarks_proto.landmark.extend([
-          landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-        ])
-        solutions.drawing_utils.draw_landmarks(
-          annotated_image,
-          hand_landmarks_proto,
-          solutions.hands.HAND_CONNECTIONS,
-          solutions.drawing_styles.get_default_hand_landmarks_style(),
-          solutions.drawing_styles.get_default_hand_connections_style())
-        height, width, _ = annotated_image.shape
-        x_coordinates = [landmark.x for landmark in hand_landmarks]
-        y_coordinates = [landmark.y for landmark in hand_landmarks]
-        text_x = int(min(x_coordinates) * width)
-        text_y = int(min(y_coordinates) * height) - MARGIN
-        cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-    return annotated_image
-
-base_options = python.BaseOptions(model_asset_path=model_path)
-options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2, min_hand_detection_confidence=0.5, min_hand_presence_confidence = 0.5, min_tracking_confidence=0.5)
-detector = vision.HandLandmarker.create_from_options(options)
-GestureRecognizer = mp.tasks.vision.GestureRecognizer
-GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
-GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
-VisionRunningMode = mp.tasks.vision.RunningMode
-
-def get_annotation_from(frame):
-    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-    detection_result = detector.detect(image)
-    if detection_result.handedness:
-        print(detection_result.handedness)
-    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-    return detection_result, annotated_image
 
 while True:
 
